47e6932e061531ec33e9c4fb00b0e95bc956e0e2
Adam Cox <adamcox.dev@gmail.com>
2022-10-25 10:32:43 -0600

    Little Snowman

---
+++ snowman.py
@@ -0,0 +1,20 @@
+# Adam Cox
+# I love Hockey
+
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
